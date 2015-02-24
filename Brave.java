ee1099f5450eb7ad615a63f89cdc2defa9daa751
Lucas Adams <lucas_adams@brown.edu>
2015-02-24 17:28:02 -0500

    testing collaboration

--- Brave.java
+++ Brave.java
@@ -2,11 +2,9 @@ package grailgames;
 
 class Brave extends Dueler {
 	
-<<<<<<< HEAD
+
 	//TESTING THIS IS A TEST
-	
-=======
->>>>>>> e220107fc59093e3e277cab11b7aa3236c638460
+
 	Brave(String name, int maxHP, int currentHP, int XP, int turn) {
 		super(name,maxHP,currentHP,XP,turn);
 	}
