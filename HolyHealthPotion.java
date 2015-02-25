e8853b3247403fa7cebab729910b1c4390e22f22
Lucas Adams <lucas_adams@brown.edu>
2015-02-24 20:57:24 -0500

    added parser

--- HolyHealthPotion.java
+++ HolyHealthPotion.java
@@ -1,7 +1,9 @@
 package grailgames;
 
-abstract class HolyHealthPotion extends Training {
-	HolyHealthPotion(String name) {
+class HolyHealthPotion extends Training {
+	
+	
+	public HolyHealthPotion(String name) {
 		super(name);
 	}
 	void effect(Dueler d) {
--- Util.java
+++ Util.java
@@ -4,7 +4,37 @@ public class Util {
 	
 	
 	public static Card parse(String cardName) {
-		return null;
+		Card card = null;
+		if (cardName == "Page Robin"){
+			card = new PageRobin();
+		} else if (cardName == "Peasant Merek") {
+			card = new PeasantMerek();
+		} else if (cardName == "Page Lancelot") {
+			card = new PageLancelot();
+		} else if (cardName == "Peasant Concorde") {
+			card = new PeasantConcorde();
+		} else if (cardName == "Page Galahad") {
+			card = new PageGalahad();
+		} else  if (cardName == "Peasant Rowan") {
+			card = new PeasantRowan();
+		} else if (cardName == "Anthrax Asylum") {
+			card = new AnthraxAsylum();
+		} else if (cardName == "Cowardly Health") {
+			card = new HolyHealthPotion("Cowardly");
+		} else if (cardName == "Jousting Practice") {
+			card = new JoustingPractice();
+		} else if (cardName == "Camelot ReinForcements") {
+			card = new CamelotReinforcements();
+		} else if (cardName == "Status Advance")  {
+			card = new StatusAdvance();
+		} else if (cardName == "Boy Arthur") {
+			card = new BoyArthur();
+		} else if (cardName == "Peasant Patsy") {
+			card = new PeasantPatsy();
+		} else if (cardName == "Bridge of Death") {
+			card = new BridgeOfDeath();
+		}
+			
 	}
 
 }
