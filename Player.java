7f8999dfbc5ba9cb25df6c49d62e4345e1753e53
Lucas Adams <lucas_adams@brown.edu>
2015-02-24 21:42:55 -0500

    added player.hand

--- Player.java
+++ Player.java
@@ -1,20 +1,33 @@
 package grailgames;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class Player {
 	
 	String name;
 	String[] deck;
-	private Card[] hand;
+	private int deckIndex = 0;
+	LinkedList<Card> hand = new LinkedList<Card>();
 	
 	public Player(String name, String deckFileName) throws IOException {
 		this.name = name;
 		this.deck = GrailIO.getDeck(deckFileName);	
+
 		
 	}
 	
 	public void draw() {
+		if (deckIndex < deck.length) {
+			try {
+				Card card = Util.parse(deck[deckIndex]);
+				hand.addFirst(card);
+				deckIndex ++;
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}		
 		
-	}
+	
 }
